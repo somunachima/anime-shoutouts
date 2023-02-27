c13a35e9c02a4f0c73757c1ab139797c8660b96e
Som Akudu <somakudu@gmail.com>
2023-02-27 19:47:47 +0000

    fix Can't resolve '@next/font/google/target.css? bug

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,12 @@
 import "./globals.css"
 import Nav from "./auth/Nav"
+import { Montserrat } from "next/font/google"
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-montserrat",
+})
 
 export default function RootLayout({ children }) {
   return (
@@ -9,7 +16,7 @@ export default function RootLayout({ children }) {
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
+      <body className={`mx-4 md:mx-48 xl:mx-96 ${montserrat.variable} font-sans bg-gray-200`}>
         <Nav />
         {children}
       </body>
