0dc3765c0f585fc5cd153da5e97a81bbe83e2e27
Som Akudu <somakudu@gmail.com>
2023-02-27 20:17:40 +0000

    add styling to login button using tailwind

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -5,7 +5,7 @@ import {signIn} from 'next-auth/react'
 export default function Login() {
   return (
       <li className="list-none">
-        <button onClick={() => signIn()} className="text-sm">
+        <button onClick={() => signIn()} className="text-sm bg-slate-900 text-white py-2 px-6 rounded-xl disabled:opacity-25 hover:bg-slate-700 -my-2.5 ml-8">
           Sign In
         </button>
       </li>
