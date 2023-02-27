3a1bcd1b6cfb82506d0795996c422ba184a0344f
Som Akudu <somakudu@gmail.com>
2023-02-27 19:25:45 +0000

    fix undefined is not valid JSON Call Stack JSON.parse syntax error

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,7 @@
-import './globals.css'
+import "./globals.css"
+import Nav from "./auth/Nav"
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/*
@@ -12,7 +9,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
   )
 }
