08e99fa0bfe9a56290c819dac25cf69a3c6fedf2
Som Akudu <somakudu@gmail.com>
2023-02-28 19:14:04 +0000

    create an edit function and prop

--- pages/api/posts/authPosts.tsx
+++ pages/api/posts/authPosts.tsx
@@ -8,7 +8,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if(req.method ==="GET"){
+  if(req.method === "GET") {
     const session = await getServerSession(req, res, authOptions)
     if (!session) {
       return res.status(401).json({ message: "Please sign in" })
