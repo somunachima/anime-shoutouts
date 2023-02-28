2208038fbfd61ca8a5c7d6bc5cde87847a15d036
Som Akudu <somakudu@gmail.com>
2023-02-28 15:23:27 +0000

    update a message that pops up to say that the post submission is loading

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -7,6 +7,7 @@ import toast from "react-hot-toast"
 export default function CreatePost(){
   const [title, setTitle] = useState("")
   const [isDisabled, setIsDisabled] = useState(false)
+  let toastPostID: string
 
   //CREATE POST
   const {mutate} = useMutation(
@@ -14,12 +15,12 @@ export default function CreatePost(){
     {
       onError: (error) => {
         if (error instanceof AxiosError) {
-          toast.error(error?.response?.data.message)
+          toast.error(error?.response?.data.message, {id: toastPostID})
         }
         setIsDisabled(false)
       },
       onSuccess: (data) => {
-        toast.success("Post has been made 🔥")
+        toast.success("Post has been made 🔥", {id: toastPostID})
         setTitle("")
         setIsDisabled(false)
       },
@@ -28,6 +29,7 @@ export default function CreatePost(){
 
   const submitPost = async (e: React.FormEvent) => {
     e.preventDefault()
+    toastPostID = toast.loading("Creating your post", {id: toastPostID})
     setIsDisabled(true)
     mutate(title)
   }
